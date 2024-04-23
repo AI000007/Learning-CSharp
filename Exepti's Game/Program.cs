c2ecade5ce36812bd83f55eae018731ecdca11dd
AI000007 <155909649+AI000007@users.noreply.github.com>
2024-04-23 23:21:36 +0100

    Add files via upload

---
+++ Exepti's Game/Program.cs
@@ -0,0 +1,28 @@
+﻿
+Random random = new Random();
+int oatmeal = random.Next(0, 10);
+int pnum = 1;
+
+List<int> chosen = new List<int>();
+
+while (true)
+{
+    Console.Write($"Player {pnum}, choose a number: ");
+    int num = Convert.ToInt16(Console.ReadLine());
+    try
+    {
+        if (num == oatmeal) throw new Exception();
+
+        if (chosen.Contains(num)) Console.WriteLine("That has already been chosen");
+        else
+        {
+            chosen.Add(num);
+            pnum = (pnum == 1) ? 2 : 1;
+        }
+    }
+
+    catch
+    {
+        Console.WriteLine($"Player {pnum} has eaten the oatmeal cookie and loses!"); break;
+    }
+}
---
+++ Guess the Number/Program.cs
@@ -0,0 +1,41 @@
+﻿
+
+using static System.Net.Mime.MediaTypeNames;
+
+int guess = -1;
+int number = -1;
+while (number < 0 || number > 100)
+{
+    number = AskForNumberInRange("User 1, enter a number between 0 and 100: ", 0, 100);
+
+}
+
+Console.Clear();
+
+while (guess != number)
+{
+    Console.Write("User 2, enter a number between 0 and 100: ");
+    guess = AskForNumber("");
+    if (guess > number) Console.WriteLine("That is too big ");
+    else if (guess < number) Console.WriteLine("That is too small ");
+}
+
+Console.WriteLine("Well done! You guessed the number! ");
+
+int AskForNumber(string text)
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int num;
+    do
+    {
+        num = AskForNumber(text);
+
+    }
+    while (num > max || num < min);
+    return num;
+}
