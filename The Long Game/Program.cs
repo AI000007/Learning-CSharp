d830eade0a5c6558ce55438432c41144256eeedf
AI000007 <155909649+AI000007@users.noreply.github.com>
2024-04-23 23:31:34 +0100

    Add files via upload

---
+++ The Long Game/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Please enter your name: ");
+string? name = Console.ReadLine();
+long score = 0;
+
+if (File.Exists($"{name}.txt"))
+{
+    score = Convert.ToInt64(File.ReadAllText($"{name}.txt"));
+}
+
+
+while (Console.ReadKey(true).Key != ConsoleKey.Enter)
+{
+    score++;
+    Console.WriteLine($"Score : {score}");
+
+}
+
+File.WriteAllText($"{name}.txt", score.ToString());
---
+++ The Old Robot/Program.cs
@@ -0,0 +1,92 @@
+﻿
+Robot robot = new();
+Console.WriteLine("Enter 3 commands:");
+
+for (int i = 0; i < 3; i++)
+{
+    string? command = Console.ReadLine();
+    RobotCommand? command2 = command switch
+    {
+        "on" => new OnCommand(),
+        "off" => new OffCommand(),
+        "north" => new NorthCommand(),
+        "east" => new EastCommand(),
+        "south" => new SouthCommand(),
+        "west" => new WestCommand(),
+        _ => null
+    };
+    if (command2 != null) robot.Commands[i] = command2;
+    else { Console.WriteLine("That is not a command. Try again: "); i--; }
+}
+
+robot.Run();
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; } = false;
+    public RobotCommand?[] Commands { get; } = new RobotCommand?[3];
+    public void Run()
+    {
+        foreach (RobotCommand? command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+
+public class NorthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered == true) robot.Y += 1;
+    }
+}
+
+public class SouthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered == true) robot.Y += -1;
+    }
+}
+
+public class EastCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered == true) robot.X += 1;
+    }
+}
+
+public class WestCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered == true) robot.X += -1;
+    }
+}
