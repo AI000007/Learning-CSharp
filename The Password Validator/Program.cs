bcb58c94b2b1ec5329ddc171053df020998bf4de
AI000007 <155909649+AI000007@users.noreply.github.com>
2024-04-23 23:37:14 +0100

    Add files via upload

---
+++ The Password Validator/Program.cs
@@ -0,0 +1,46 @@
+﻿
+while (true)
+{
+    Console.Write("Enter a password: ");
+    string password = Console.ReadLine();
+    PasswordValidator passwordValid = new() { Password = password };
+    if (passwordValid.IsValid()) Console.WriteLine("Valid");
+    else Console.WriteLine("Invalid");
+}
+
+class PasswordValidator
+{
+    public string Password { get; init; }
+    public bool LengthCheck() => (Password.Length >= 6 && Password.Length <= 13);
+    public bool ContainsUpper()
+    {
+        foreach (char c in Password) if (char.IsUpper(c)) return true;
+        return false;
+
+    }
+    
+   
+    public bool ContainsLower()
+    {
+        foreach (char c in Password) if (char.IsLower(c)) return true;
+        return false;
+
+    }
+
+    public bool ContainsDigit()
+    {
+        foreach (char c in Password) if (char.IsDigit(c)) return true;
+        return false;
+
+    }
+
+    public bool ContainsTOrAmpersand()
+    {
+        foreach (char c in Password) if (c == Convert.ToChar("T") || c == Convert.ToChar("&")) return true;
+        return false;
+
+    }
+
+    public bool IsValid() => ContainsDigit() && ContainsUpper() && ContainsLower() && !ContainsTOrAmpersand() && LengthCheck();
+
+}
---
+++ The Point/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Point point1 = new Point(2, 3);
+Point point2 = new Point(-4, 0);
+Point point3 = new Point();
+
+Console.WriteLine($"({point1.X}, {point1.Y}) ({point2.X}, {point2.Y}) ({point3.X}, {point3.Y}) ");
+
+class Point
+{
+    public float X { get; private set; }
+    public float Y { get; private set; }
+
+    public Point(float x, float y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public Point()
+    {
+        X = 0; Y = 0;
+    }
+}
